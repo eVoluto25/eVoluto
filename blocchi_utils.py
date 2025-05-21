e9c39325c009f109515711270d5bed9a1d55bca0
eVoluto25 <info@capitaleaziendale.it>
2025-05-21 16:38:10 +0200

    Update blocchi_utils.py

--- blocchi_utils.py
+++ blocchi_utils.py
@@ -6,7 +6,7 @@ def suddividi_blocchi_coerenti(testo: str, max_lunghezza: int = 4500) -> list[st
     Divide il testo in blocchi coerenti (per paragrafi), senza tagli casuali,
     rispettando i limiti di token (approssimati in caratteri).
     '''
-    paragrafi = re.split(r'\n{2,}', testo)
+    paragrafi = re.split(r'\n{2,}', '\n'.join(testo))
     blocchi = []
     blocco_corrente = ""
 
